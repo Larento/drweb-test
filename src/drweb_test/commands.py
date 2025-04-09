110ec8e413e97880011f1fbcafd977c3fbb2f4e7
Иларион Ерченко <turolretar@gmail.com>
2025-04-09 13:41:20 +0300

    :sparkles: add commands module

---
+++ src/drweb_test/commands.py
@@ -0,0 +1,97 @@
+import abc
+from typing import Generic, TypeVar
+
+from drweb_test.database import Database
+
+__all__ = (
+    "NoValue",
+    "CommandError",
+    "AbstractCommand",
+    "SetValueForKeyCommand",
+    "GetValueByKeyCommand",
+    "UnsetValueForKeyCommand",
+    "CountValueReferencesCommand",
+    "FindKeysWithValueCommand",
+)
+
+
+class NoValue:
+    """
+    В базе отсутствует значение для данного ключа.
+    """
+
+
+class CommandError(Exception):
+    """
+    При выполнении команды произошла ошибка.
+    """
+
+
+CommandResultT = TypeVar("CommandResultT")
+
+
+class AbstractCommand(abc.ABC, Generic[CommandResultT]):
+    def __init__(self, *args: str):
+        self.args = args
+
+    @abc.abstractmethod
+    def __call__(self, db: Database) -> CommandResultT: ...
+
+
+class SetValueForKeyCommand(AbstractCommand[None]):
+    def __call__(self, db: Database):
+        try:
+            key, value = self.args
+        except ValueError:
+            raise CommandError("Команда ожидает 2 аргумента.")
+
+        if not value:
+            raise CommandError("Значение не может быть пустым.")
+
+        db.set(key, value)
+
+
+class GetValueByKeyCommand(AbstractCommand[str | NoValue]):
+    def __call__(self, db: Database):
+        try:
+            (key,) = self.args
+        except ValueError:
+            raise CommandError("Команда ожидает 1 аргумент.")
+
+        return db.get(key, NoValue())
+
+
+class UnsetValueForKeyCommand(AbstractCommand[None]):
+    def __call__(self, db: Database):
+        try:
+            (key,) = self.args
+        except ValueError:
+            raise CommandError("Команда ожидает 1 аргумент.")
+
+        db.unset(key)
+
+
+class CountValueReferencesCommand(AbstractCommand[int]):
+    def __call__(self, db: Database):
+        try:
+            (value,) = self.args
+        except ValueError:
+            raise CommandError("Команда ожидает 1 аргумент.")
+
+        if not value:
+            raise CommandError("Значение не может быть пустым.")
+
+        return len(db.find_keys_with_value(value))
+
+
+class FindKeysWithValueCommand(AbstractCommand[list[str]]):
+    def __call__(self, db: Database):
+        try:
+            (value,) = self.args
+        except ValueError:
+            raise CommandError("Команда ожидает 1 аргумент.")
+
+        if not value:
+            raise CommandError("Значение не может быть пустым.")
+
+        return db.find_keys_with_value(value)
---
+++ tests/commands.py
@@ -0,0 +1,89 @@
+import unittest
+
+from drweb_test import commands
+from drweb_test.database import Database
+
+
+class TestCommands(unittest.TestCase):
+    def setUp(self):
+        self.db = Database()
+
+    def test_set_value_for_key_command(self):
+        command = commands.SetValueForKeyCommand("a", "10")
+        assert command(self.db) is None
+        assert self.db.get("a") == "10"
+
+    def test_get_value_by_key_command(self):
+        self.db.set("a", "10")
+        command = commands.GetValueByKeyCommand("a")
+        assert command(self.db) == "10"
+        assert self.db.get("a") == "10"
+
+        command = commands.GetValueByKeyCommand("b")
+        assert isinstance(command(self.db), commands.NoValue)
+
+    def test_unset_value_for_key_command(self):
+        self.db.set("a", "10")
+        command = commands.UnsetValueForKeyCommand("a")
+        assert command(self.db) is None
+        assert self.db.get("a") is None
+
+    def test_count_value_references_command(self):
+        self.db.set("a", "10")
+        self.db.set("b", "10")
+        self.db.set("c", "10")
+        command = commands.CountValueReferencesCommand("10")
+        assert command(self.db) == 3
+
+    def test_find_keys_with_value_command(self):
+        self.db.set("d", "10")
+        self.db.set("e", "10")
+        self.db.set("f", "10")
+        command = commands.FindKeysWithValueCommand("10")
+        assert command(self.db) == ["d", "e", "f"]
+
+    def test_not_enough_arguments(self):
+        with self.assertRaises(commands.CommandError):
+            commands.SetValueForKeyCommand()(self.db)
+
+        with self.assertRaises(commands.CommandError):
+            commands.GetValueByKeyCommand()(self.db)
+
+        with self.assertRaises(commands.CommandError):
+            commands.UnsetValueForKeyCommand()(self.db)
+
+        with self.assertRaises(commands.CommandError):
+            commands.CountValueReferencesCommand()(self.db)
+
+        with self.assertRaises(commands.CommandError):
+            commands.FindKeysWithValueCommand()(self.db)
+
+    def test_too_many_arguments(self):
+        with self.assertRaises(commands.CommandError):
+            commands.SetValueForKeyCommand("a", "10", "20")(self.db)
+
+        with self.assertRaises(commands.CommandError):
+            commands.GetValueByKeyCommand("a", "10")(self.db)
+
+        with self.assertRaises(commands.CommandError):
+            commands.UnsetValueForKeyCommand("a", "10")(self.db)
+
+        with self.assertRaises(commands.CommandError):
+            commands.CountValueReferencesCommand("a", "10")(self.db)
+
+        with self.assertRaises(commands.CommandError):
+            commands.FindKeysWithValueCommand("a", "10")(self.db)
+
+    def test_empty_value_argument(self):
+        with self.assertRaises(commands.CommandError):
+            commands.SetValueForKeyCommand("a", "")(self.db)
+
+        with self.assertRaises(commands.CommandError):
+            commands.CountValueReferencesCommand("a", "")(self.db)
+
+        with self.assertRaises(commands.CommandError):
+            commands.FindKeysWithValueCommand("a", "")(self.db)
+
+
+if __name__ == "__main__":
+    unittest.main()
